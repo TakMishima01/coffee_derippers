7150c4aad80bdddf81f2f9da8b2d2a0774a7c0cc
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-01-04 15:36:05 +0900

    [Add]active_storageインストール

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,23 +1,38 @@
 class Public::RecipesController < ApplicationController
   def index
   end
-  
+
   def show
   end
-  
+
   def new
+    @recipe = Recipe.new
   end
-  
+
   def create
+    recipe = Recipe.new(redipe_params)
+    if recipe.save
+      redirect_to recipe_path(recipe), notice:  "登録が完了しました"
+    else
+      flash.now[:error] = "空欄があります"
+      render :new
+    end
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:user_id, :name, :coffee_beans_name, :roast, :amount,
+    :grind, :temperature, :total_pouring_amount, :extraction_amount, :total_extraction_time, :dripper, :paper, :point, :status)
+  end
+
 end
