65eaeac7f2b40b84a5f76f2051dca503e567c948
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-02-23 23:45:54 +0900

    [Add]レイアウト途中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @production_areas = ProductionArea.all
+    @recipe_rank = Recipe.find(MyRecipe.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
   end
 
   def about
