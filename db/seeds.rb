25e14d42adff4394ef90f932d763412b5ae68eaf
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-03-21 14:55:35 +0900

    [fix]レイアウト調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,25 +11,33 @@
 
 users = User.create!(
   [
-    {email:'john_doe@example.com', name: 'john_doe', password: 'testpassword'},
-    {email:'janesmith@example.com', name: 'janesmith', password: 'testpassword'},
-    {email:'mike_jones@example.com', name: 'mike_jones', password: 'testpassword'},
-    {email:'sara_kim@example.com', name: 'sara_kim', password: 'testpassword'},
-    {email:'david_lee@example.com', name: 'david_lee', password: 'testpassword'},
-    {email:'mary_smith@example.com', name: 'mary_smith', password: 'testpassword'},
-    {email:'jason_kim@example.com', name: 'jason_kim', password: 'testpassword'},
-    {email:'emily_wang@example.com', name: 'emily_wang', password: 'testpassword'},
-    {email:'peter_nguyen@example.com', name: 'peter_nguyen', password: 'testpassword'},
-    {email:'stephanie_choi@example.com', name: 'stephanie_choi', password: 'testpassword'},
-    {email:'kevin_kang@example.com', name: 'kevin_kang', password: 'testpassword'},
-    {email:'jennifer_park@example.com', name: 'jennifer_park', password: 'testpassword'},
-    {email:'joshua_lee@example.com', name: 'joshua_lee', password: 'testpassword'},
-    {email:'kristen_kim@example.com', name: 'kristen_kim', password: 'testpassword'},
-    {email:'benjamin_wong@example.com', name: 'benjamin_wong', password: 'testpassword'},
-    {email:'rachel_kim@example.com', name: 'rachel_kim', password: 'testpassword'},
-    {email:'andrew_lee@example.com', name: 'andrew_lee', password: 'testpassword'},
-    {email:'lisa_wang@example.com', name: 'lisa_wang', password: 'testpassword'},
-    {email:'samuel_kim@example.com', name: 'samuel_kim', password: 'testpassword'},
-    {email:'julia_choi@example.com', name: 'julia_choi', password: 'testpassword'}
+    {email:'john_doe@example.com', name: 'john_doe', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email:'jane_smith@example.com', name: 'jane_smith', password: 'testpassword' ,profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email:'mike_jones@example.com', name: 'mike_jones', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")},
+    {email:'sara_kim@example.com', name: 'sara_kim', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")},
+    {email:'david_lee@example.com', name: 'david_lee', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")},
+    {email:'mary_smith@example.com', name: 'mary_smith', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user6.jpg"), filename:"sample-user6.jpg")},
+    {email:'jason_kim@example.com', name: 'jason_kim', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user7.jpg"), filename:"sample-user7.jpg")},
+    {email:'emily_wang@example.com', name: 'emily_wang', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user8.jpg"), filename:"sample-user8.jpg")},
+    {email:'peter_nguyen@example.com', name: 'peter_nguyen', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user9.jpg"), filename:"sample-user9.jpg")},
+    {email:'kevin_kang@example.com', name: 'kevin_kang', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user10.jpg"), filename:"sample-user10.jpg")},
+    {email:'stephanie_choi@example.com', name: 'stephanie_choi', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user11.jpg"), filename:"sample-user11.jpg")},
+    {email:'jennifer_park@example.com', name: 'jennifer_park', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user12.jpg"), filename:"sample-user12.jpg")},
+    {email:'joshua_lee@example.com', name: 'joshua_lee', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user13.jpg"), filename:"sample-user13.jpg")},
+    {email:'kristen_kim@example.com', name: 'kristen_kim', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user14.jpg"), filename:"sample-user14.jpg")},
+    {email:'benjamin_wong@example.com', name: 'benjamin_wong', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user15.jpg"), filename:"sample-user15.jpg")},
+    {email:'rachel_kim@example.com', name: 'rachel_kim', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user16.jpg"), filename:"sample-user16.jpg")},
+    {email:'andrew_lee@example.com', name: 'andrew_lee', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user17.jpg"), filename:"sample-user17.jpg")},
+    {email:'lisa_wang@example.com', name: 'lisa_wang', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user18.jpg"), filename:"sample-user18.jpg")},
+    {email:'samuel_kim@example.com', name: 'samuel_kim', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user19.jpg"), filename:"sample-user19.jpg")},
+    {email:'julia_choi@example.com', name: 'julia_choi', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user20.jpg"), filename:"sample-user20.jpg")}
   ]
 )
+
+recipes = Recipe.create!(
+  [
+    {email: 'john_doe@example.com', name: 'john_doe', password: 'testpassword', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {production_area_id: 1, user_id: 1, name: 'エチオピアモカ', coffee_beans_name: 'タリーズエチオピアモカ', roast: 1, amount: 20, grind: 2, temperature: 90, total_pouring_amount: 300, extraction_amount: 270, total_extraction_time: 180, dripper: 'V60', paper: '純正', point: 'test data', status: 'true', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-recipe1.jpg"), filename:"sample-recipe1.jpg")},
+  ]
+)
+
