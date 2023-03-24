76e7db15ae56369f5465e6c2b6c6b04fb8605a34
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-03-24 16:50:39 +0900

    [fix]seedの編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -341,3 +341,53 @@ recipe_comments = RecipeComment.create!(
   ]
 )
 
+my_recipes = MyRecipe.create!(
+  [
+    {recipe_id: 7, user_id: 1}, {recipe_id: 24, user_id: 1}, {recipe_id: 16, user_id: 1}, {recipe_id: 9, user_id: 1}, {recipe_id: 2, user_id: 1},
+    {recipe_id: 21, user_id: 2}, {recipe_id: 10, user_id: 2}, {recipe_id: 30, user_id: 2}, {recipe_id: 18, user_id: 2}, {recipe_id: 3, user_id: 2},
+    {recipe_id: 15, user_id: 3}, {recipe_id: 23, user_id: 3}, {recipe_id: 26, user_id: 3}, {recipe_id: 27, user_id: 3}, {recipe_id: 5, user_id: 3},
+    {recipe_id: 31, user_id: 4}, {recipe_id: 13, user_id: 4}, {recipe_id: 8, user_id: 4}, {recipe_id: 22, user_id: 4}, {recipe_id: 17, user_id: 4},
+    {recipe_id: 12, user_id: 5}, {recipe_id: 14, user_id: 5}, {recipe_id: 25, user_id: 5}, {recipe_id: 29, user_id: 5}, {recipe_id: 6, user_id: 5},
+    {recipe_id: 32, user_id: 6}, {recipe_id: 19, user_id: 6}, {recipe_id: 4, user_id: 6}, {recipe_id: 28, user_id: 6}, {recipe_id: 11, user_id: 6},
+    {recipe_id: 20, user_id: 7}, {recipe_id: 1, user_id: 7}, {recipe_id: 33, user_id: 7}, {recipe_id: 3, user_id: 7}, {recipe_id: 16, user_id: 7},
+    {recipe_id: 22, user_id: 8}, {recipe_id: 25, user_id: 8}, {recipe_id: 7, user_id: 8}, {recipe_id: 11, user_id: 8}, {recipe_id: 31, user_id: 8},
+    {recipe_id: 8, user_id: 9}, {recipe_id: 12, user_id: 9}, {recipe_id: 27, user_id: 9}, {recipe_id: 15, user_id: 9}, {recipe_id: 29, user_id: 9},
+    {recipe_id: 6, user_id: 10}, {recipe_id: 2, user_id: 10}, {recipe_id: 26, user_id: 10}, {recipe_id: 18, user_id: 10}, {recipe_id: 30, user_id: 10},
+    {recipe_id: 23, user_id: 11}, {recipe_id: 10, user_id: 11}, {recipe_id: 21, user_id: 11}, {recipe_id: 5, user_id: 11}, {recipe_id: 13, user_id: 11},
+    {recipe_id: 3, user_id: 12},{recipe_id: 27, user_id: 12},{recipe_id: 14, user_id: 12},{recipe_id: 9, user_id: 12},{recipe_id: 23, user_id: 12},
+    {recipe_id: 6, user_id: 13},{recipe_id: 28, user_id: 13},{recipe_id: 18, user_id: 13},{recipe_id: 4, user_id: 13},{recipe_id: 16, user_id: 13},
+    {recipe_id: 12, user_id: 14},{recipe_id: 31, user_id: 14},{recipe_id: 20, user_id: 14},{recipe_id: 7, user_id: 14},{recipe_id: 33, user_id: 14},
+    {recipe_id: 28, user_id: 15},{recipe_id: 12, user_id: 15},{recipe_id: 4, user_id: 15},{recipe_id: 31, user_id: 15},{recipe_id: 10, user_id: 15},
+    {recipe_id: 6, user_id: 16},{recipe_id: 22, user_id: 16},{recipe_id: 20, user_id: 16},{recipe_id: 8, user_id: 16},{recipe_id: 13, user_id: 16},{recipe_id: 16, user_id: 16},
+    {recipe_id: 6, user_id: 17},{recipe_id: 17, user_id: 17},{recipe_id: 22, user_id: 17},{recipe_id: 2, user_id: 17},{recipe_id: 11, user_id: 17},{recipe_id: 29, user_id: 17},
+    {recipe_id: 31, user_id: 18},{recipe_id: 9, user_id: 18},{recipe_id: 16, user_id: 18},{recipe_id: 26, user_id: 18},{recipe_id: 19, user_id: 18},
+    {recipe_id: 5, user_id: 19},{recipe_id: 14, user_id: 19},{recipe_id: 3, user_id: 19},{recipe_id: 31, user_id: 19},{recipe_id: 21, user_id: 19},{recipe_id: 23, user_id: 19},
+    {recipe_id: 7, user_id: 20},{recipe_id: 24, user_id: 20},{recipe_id: 32, user_id: 20},{recipe_id: 15, user_id: 20},{recipe_id: 1, user_id: 20},{recipe_id: 33, user_id: 20},
+  ]
+)
+
+relationships = Relationship.create!(
+  [
+    {followed_id: 1, follower_id: 18},{followed_id: 1, follower_id: 5},{followed_id: 1, follower_id: 15},{followed_id: 1, follower_id: 11},{followed_id: 1, follower_id: 14},
+    {followed_id: 2, follower_id: 7},{followed_id: 2, follower_id: 9},{followed_id: 2, follower_id: 6},{followed_id: 2, follower_id: 15},
+    {followed_id: 3, follower_id: 7},{followed_id: 3, follower_id: 16},{followed_id: 3, follower_id: 4},{followed_id: 3, follower_id: 20},{followed_id: 3, follower_id: 12},
+    {followed_id: 4, follower_id: 8},{followed_id: 4, follower_id: 9},{followed_id: 4, follower_id: 20},
+    {followed_id: 5, follower_id: 16},{followed_id: 5, follower_id: 3},{followed_id: 5, follower_id: 13},{followed_id: 5, follower_id: 20},
+    {followed_id: 6, follower_id: 13},{followed_id: 6, follower_id: 17},{followed_id: 6, follower_id: 7},
+    {followed_id: 7, follower_id: 10},{followed_id: 7, follower_id: 19},
+    {followed_id: 8, follower_id: 5},{followed_id: 8, follower_id: 12},{followed_id: 8, follower_id: 7},{followed_id: 8, follower_id: 18},
+    {followed_id: 9, follower_id: 2},{followed_id: 9, follower_id: 5},{followed_id: 9, follower_id: 15},{followed_id: 9, follower_id: 11},{followed_id: 9, follower_id: 14},
+    {followed_id: 10, follower_id: 6},{followed_id: 10, follower_id: 16},{followed_id: 10, follower_id: 19},{followed_id: 10, follower_id: 8},
+    {followed_id: 11, follower_id: 18},{followed_id: 11, follower_id: 7},{followed_id: 11, follower_id: 12},{followed_id: 11, follower_id: 14},{followed_id: 11, follower_id: 17},
+    {followed_id: 12, follower_id: 4},{followed_id: 12, follower_id: 9},{followed_id: 12, follower_id: 13},{followed_id: 12, follower_id: 15},{followed_id: 12, follower_id: 17},
+    {followed_id: 13, follower_id: 18},{followed_id: 13, follower_id: 16},{followed_id: 13, follower_id: 20},{followed_id: 13, follower_id: 10},
+    {followed_id: 14, follower_id: 19},{followed_id: 14, follower_id: 8},{followed_id: 14, follower_id: 11},
+    {followed_id: 15, follower_id: 10},{followed_id: 15, follower_id: 19},{followed_id: 15, follower_id: 6},
+    {followed_id: 16, follower_id: 1},{followed_id: 16, follower_id: 4},{followed_id: 16, follower_id: 9},{followed_id: 16, follower_id: 14},{followed_id: 16, follower_id: 18},
+    {followed_id: 17, follower_id: 1},{followed_id: 17, follower_id: 12},{followed_id: 17, follower_id: 16},{followed_id: 17, follower_id: 20},
+    {followed_id: 18, follower_id: 5},{followed_id: 18, follower_id: 8},{followed_id: 18, follower_id: 9},
+    {followed_id: 19, follower_id: 3},{followed_id: 19, follower_id: 6},{followed_id: 19, follower_id: 11},{followed_id: 19, follower_id: 15},{followed_id: 19, follower_id: 20},
+    {followed_id: 20, follower_id: 1},{followed_id: 20, follower_id: 7},{followed_id: 20, follower_id: 11},{followed_id: 20, follower_id: 15}
+  ]
+)
+
